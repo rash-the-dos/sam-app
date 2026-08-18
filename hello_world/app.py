8acf31ad117625fcece0419b795f9bc1d117f88d
Patrick Rashidi <prashidi@amazon.com>
2026-08-18 14:04:08 +0100

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,6 +6,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Patrick",
+            "message": "I'm using canary deployments",
         }),
     }
